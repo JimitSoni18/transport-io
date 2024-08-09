89b3d5cdd25324febed4f61a739b395b34823da7
jimit <jimitsoni18@gmail.com>
2024-08-09 19:57:57 +0530

    removed claude, added framing - wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,8 @@
 pub mod eventful;
-pub mod claude;
+pub mod framing;
+pub use wtransport::*;
 
-pub use wtransport::ServerConfig;
-use wtransport::{Connection, Endpoint};
 
-pub struct Server {
-	endpoint_server: Endpoint<wtransport::endpoint::endpoint_side::Server>,
-}
-
-pub struct TransportConnection {
-	connection: wtransport::connection::Connection,
-}
-
-impl TransportConnection {
-	#[inline]
-	pub async fn accept_bi(
-		&self,
-	) -> Result<
-		(wtransport::SendStream, wtransport::RecvStream),
-		wtransport::error::ConnectionError,
-	> {
-		self.connection.accept_bi().await
-	}
-
-	#[inline]
-	pub async fn accept_uni(
-		&self,
-	) -> Result<wtransport::RecvStream, wtransport::error::ConnectionError> {
-		self.connection.accept_uni().await
-	}
-}
-
-impl Server {
-	pub fn new(server_config: ServerConfig) -> std::io::Result<Self> {
-		let endpoint_server = Endpoint::server(server_config)?;
-		Ok(Self { endpoint_server })
-	}
-
-	pub async fn on_connection(
-		self,
-		_f: impl Fn(Connection),
-	) -> Result<TransportConnection, wtransport::error::ConnectionError> {
-		let connection =
-			self.endpoint_server.accept().await.await?.accept().await?;
-		Ok(TransportConnection { connection })
-	}
-}
 
 #[cfg(test)]
 mod tests {}
